006d36b2bd4419fec687f113bde5d37b11507c50
Loki31490 <95440838+Loki31490@users.noreply.github.com>
2025-08-25 19:19:44 +0200

    Add data to table mockup

--- src/db_handler/database.rs
+++ src/db_handler/database.rs
@@ -1,22 +1,37 @@
-use log::{error, info, trace};
-use rusqlite::Connection;
+use log::{error, info};
+use rusqlite::{Connection, Error, Result, params};
 use std::path::PathBuf;
 
 pub struct Database {
-    pub path: PathBuf,
+    path: PathBuf,
 }
 
 impl Database {
-    pub fn from(path: PathBuf) -> Self {
-        trace!("\nNEW DATABASE\nfrom path: {:?}", &path);
+    //create a table
+    pub fn new(path: PathBuf) -> Self {
+        let conn = Connection::open(&path).expect("connection not granted");
+        let stmt = conn.execute(
+            "
+        CREATE TABLE IF NOT EXISTS test_table (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL)",
+            (),
+        );
+        match stmt {
+            Ok(num) => info!("New test_table created at path: \n{:?}", &path),
+            Err(e) => error!("'{e}'"),
+        }
         Self { path: path }
     }
-
-    pub fn print_db(&self) {
-        let conn = Connection::open(&self.path);
-        match conn {
-            Ok(ok) => info!("\nCONNECTION GRANTED\n{ok:?}"),
-            Err(e) => error!("\nCONNECTION NOT GRANTED\n{e}"),
-        }
+    pub fn insert_to_table(self, table: &str, column: &str, content: &str) -> Result<(), Error> {
+        let conn = Connection::open(&self.path)?;
+        let stmt = conn.execute(
+            "
+            INSERT INTO test_table (name)
+            VALUES (?3)
+",
+            params![table, column, content],
+        )?;
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
+#![allow(unused_variables)]
+
+use rusqlite::{Error, Result};
 use std::path::PathBuf;
-
 mod db_handler;
+use db_handler::Database;
 
-fn main() {
+fn main() -> Result<(), Error> {
     env_logger::init();
-    //trace!("trace");
-    //info!("info");
-    //warn!("warn");
-    //error!("error");
+    //trace!("trace"); PRIORITY = 4
+    //info!("info"); PRIORITY = 3
+    //warn!("warn"); PRIORITY = 2
+    //error!("error"); PRIORITY = 1
 
-    let path = PathBuf::from("/home/baptiste/Bureau/Rust/data_handler/data/my_data/");
-    let database = db_handler::Database::from(path);
-    database.print_db();
+    let path = PathBuf::from("/home/baptiste/Documents/Rust/data_handler/data/my_data.db3");
+    let database = Database::new(path);
+    database.insert_to_table("test_table", "name", "Joe Danger")?;
+    Ok(())
 }
