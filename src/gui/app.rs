aaf4be229fd188b84d9f36634a97da207133a9e2
Loki31490 <95440838+Loki31490@users.noreply.github.com>
2025-09-05 17:41:13 +0200

    fast save

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -1,18 +1,38 @@
 use egui_extras::{Column, TableBuilder};
+use egui_file_dialog::FileDialog;
+
+pub struct MyTestForm {
+    name: String,
+    Last_name: String,
+}
 
 #[derive(Default)]
 pub struct Appstate(String);
 
+pub struct AppConfig {
+    window_size: egui::Vec2,
+}
+
 pub struct MyApp {
     state: Appstate,
-    database: crate::db_handler::Database,
+    database: FileDialog,
+    config: AppConfig,
+    test_form: MyTestForm,
 }
 
 impl MyApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         Self {
             state: Appstate::default(),
-            database: crate::db_handler::Database::default(),
+            database: FileDialog::new(),
+            config: AppConfig {
+                window_size: egui::vec2(800.0, 600.0),
+            },
+
+            test_form: MyTestForm {
+                name: String::default(),
+                Last_name: String::default(),
+            },
         }
     }
 }
@@ -20,12 +40,28 @@ impl MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical(|ui| {
+                ui.heading("Data Handler");
+                ui.separator();
+
+                ui.label("Name");
+                ui.text_edit_singleline(&mut self.test_form.name);
+
+                ui.label("Last Name");
+                ui.text_edit_singleline(&mut self.test_form.Last_name);
+
+                ui.separator();
+                if ui.button("Add").clicked() {
+                    todo!()
+                };
+            });
+
             let table_builder =
                 TableBuilder::new(ui)
                     .columns(Column::auto(), 4)
                     .header(20.0, |mut header| {
                         header.col(|ui| {
-                            ui.checkbox(&mut true, "Select all");
+                            ui.checkbox(&mut true, "");
                         });
                         header.col(|ui| {
                             ui.strong("id");
